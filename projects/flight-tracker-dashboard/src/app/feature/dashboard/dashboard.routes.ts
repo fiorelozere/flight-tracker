0d6c2485059bbb9585e795171cd82d612de0c91a
Fiorelo Zere <47506023+fiorelozere@users.noreply.github.com>
2024-09-11 11:24:47 +0200

    Update dashboard.routes.ts

--- projects/flight-tracker-dashboard/src/app/feature/dashboard/dashboard.routes.ts
+++ projects/flight-tracker-dashboard/src/app/feature/dashboard/dashboard.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardService } from './dashboard/dashboard.service';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export default [
   {
     path: '',
-    providers: [DashboardService],
+    providers: [
+      DashboardService,
+      provideCharts(withDefaultRegisterables())
+    ],
     children: [
       {
         path: '',
